c6faa07784e238e677123ee776bc88a03019526a
mikomac405 <mikolajmacura0510@gmail.com>
2025-04-23 20:49:49 +0200

    refactor: update MQTT data handling and remove unused service file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import aiomqtt
 import asyncio
 import os
@@ -6,95 +7,43 @@ import pymysql
 from datetime import datetime
 import json
 from dotenv import load_dotenv
+from enum import IntEnum
+
+class TYPE(IntEnum):
+    FLOOD = 1
+    HOLD = 2
+
+ATTACK_TYPE = TYPE.FLOOD
 
 DEVICES = ["esp8266/inside", "esp8266/outside", "esp32/light", "esp32/window"]
 
 load_dotenv()
 
-aiomqtt.Client(
-    hostname=os.environ.get("MQTT_HOST"), port=int(os.environ.get("MQTT_PORT"))
-)
-
-# DATA
-data_query = """
-insert into safelearniot.data (device, data, time_sent, time_saved)
-values (%s, %s, %s, %s);
-"""
-# ML
-ml_query = """
-insert into ml (device, data, time_sent, time_saved)
-values (%s, %s, %s, %s);
-"""
-
-# STATUS
-status_query = """
-insert into safelearniot.status (active_time, device, status, time_sent, time_saved)
-values (%s, %s, %s, %s, %s);
-"""
-
-
 async def send_weights(client: aiomqtt.Client):
     while True:
         for device in DEVICES:
-            await client.publish(f"cmd/{device}", "weights", retain=True)
-            print(f"Published 'weights' on cmd/{device}")
+            await client.publish(f"cmd_gateway/{device}", "get_params", retain=True)
+            print(f"Published 'get_params' on cmd_gateway/{device}")
 
-        await asyncio.sleep(4 * 60 * 60)
+        await asyncio.sleep(15 * 60) # Request get_params every 15 minutes
 
 
 async def main():
-    connection = pymysql.connect(
-        host=os.environ["MYSQL_HOST"],
-        user=os.environ["MYSQL_USER"],
-        password=os.environ["MYSQL_PASS"],
-        database=os.environ["MYSQL_DB"],
-        port=int(os.environ["MYSQL_PORT"]),
-    )
-    cursor = connection.cursor()
-
-    async with aiomqtt.Client("192.168.0.164") as client:
+    async with aiomqtt.Client(hostname=os.environ.get("MQTT_HOST"), port=int(os.environ.get("MQTT_PORT"))) as client:
         asyncio.create_task(send_weights(client))
 
         for device in DEVICES:
-            await client.subscribe(f"data/{device}")
-            await client.subscribe(f"ml/{device}")
-            await client.subscribe(f"status/{device}")
+            await client.subscribe(f"cmd_mcu/{device}")
 
         async for message in client.messages:
-            data = ast.literal_eval(message.payload.decode())
-            data["time_sent"] = datetime.fromtimestamp(data["time_sent"])
-            data["time_saved"] = datetime.now()
-
-            if message.topic.matches("data/#"):
-                values = (
-                    data["device"],
-                    json.dumps(data["data"]),
-                    data["time_sent"],
-                    data["time_saved"],
-                )
-                cursor.execute(data_query, values)
-                connection.commit()
-
-            if message.topic.matches("ml/#"):
-                values = (
-                    data["device"],
-                    json.dumps(data["data"]),
-                    data["time_sent"],
-                    data["time_saved"],
-                )
-                cursor.execute(ml_query, values)
-                connection.commit()
-
-            if message.topic.matches("status/#"):
-                values = (
-                    data["active_time"],
-                    data["device"],
-                    data["status"],
-                    data["time_sent"],
-                    data["time_saved"],
-                )
-                cursor.execute(status_query, values)
-                connection.commit()
+            if message.topic.matches("cmd_mcu/#"):
+                if message.payload.decode() != "":
+                    if ATTACK_TYPE == TYPE.FLOOD: # Publish 'ok' message
+                        await client.publish(message.topic.value.replace('cmd_mcu', 'cmd_gateway'), f"set_params;ok".encode(), retain=True)
+                        await client.publish(message.topic.value, "", retain=True)
+                    elif ATTACK_TYPE == TYPE.HOLD: # Leave on hold
+                        pass
+            
 
 
 asyncio.run(main())
